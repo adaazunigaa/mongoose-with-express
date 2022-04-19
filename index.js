b9f92496ea97fa40f0978bf3183ceadf758dd29e
AdaZuniga <56266464+AdaZuniga@users.noreply.github.com>
2022-04-19 17:58:40 -0500

    added 'edit.ejs' to edit products

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const mongoose = require("mongoose");
+const methodOverride = require("method-override")
 
 const Product = require("./models/product");
 
@@ -17,6 +18,7 @@ mongoose.connect('mongodb://localhost:27017/farmStand')
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.urlencoded({extended: true}))
+app.use(methodOverride("_method"));
 
 
 
@@ -48,8 +50,18 @@ app.get("/products/:id", async (req,res)=>{
 })
 
 
+app.get("/products/:id/edit", async (req,res)=>{
+    const {id} = req.params;
+    const product = await Product.findById(id);
+    res.render("products/edit.ejs", {product});
+});
 
-
+app.put("/products/:id", async(req,res)=>{
+    // console.log(req.body);
+    const {id} = req.params;
+    const product = await Product.findByIdAndUpdate(id, req.body, {runValidators: true, new: true});
+    res.redirect(`/products/${product._id}`)
+});
 
 
 app.listen(3000, () => {
