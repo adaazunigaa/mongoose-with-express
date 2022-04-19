8a6f412b7c29cdbe6a1bd94a4a63d05b7bde5e52
AdaZuniga <56266464+AdaZuniga@users.noreply.github.com>
2022-04-18 21:45:18 -0500

    Creating a new product

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ mongoose.connect('mongodb://localhost:27017/farmStand')
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
-
+app.use(express.urlencoded({extended: true}))
 
 
 
@@ -27,6 +27,17 @@ app.get("/", (req, res)=>{
 app.get("/products", async (req,res)=>{
     const products = await Product.find({});
     res.render("products/index.ejs", {products});
+});
+
+app.get("/products/new", (req,res)=>{
+    res.render("products/new.ejs");
+});
+
+app.post("/products", async (req,res) =>{
+    const newProduct = new Product(req.body);
+    await newProduct.save();
+    // console.log(newProduct)
+    res.redirect("/products");
 });
 
 app.get("/products/:id", async (req,res)=>{
